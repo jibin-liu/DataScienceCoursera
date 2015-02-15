182bd7187c4a07f3353ed7b615e93353d313b05a
Jibin Liu <liujibin320@gmail.com>
2015-02-15 14:11:15 -0500

    Update Quiz2.R
    update Quiz2 - Q1 codes

--- Getting-and-Cleaning-Data/Quiz2.R
+++ Getting-and-Cleaning-Data/Quiz2.R
@@ -12,7 +12,10 @@ github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
 gtoken <- config(token = github_token)
 req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
 stop_for_status(req)
-str(req)
+# Transfer contents to JSON and look for DataSharing repo
+json1 = content(req)
+json2 = jsonlite::fromJSON(toJSON(json1))
+json2[, c("name", "created_at")]
 
 # Question 2
 library(sqldf)
